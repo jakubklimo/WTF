919075ca85c6876dcb9d24f0a9b46661d77945b7
Jakub Klimo <jakubklimo@b435.kolej.tul.cz>
2025-04-09 13:43:58 +0200

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# WTF (Weather: Temperature & Forecast)
---
+++ create_tables.sql
@@ -0,0 +1,33 @@
+USE WTF;
+
+CREATE TABLE country(
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE city(
+    id int NOT NULL AUTO_INCREMENT,
+    name VARCHAR(200) NOT NULL,
+    latitude DECIMAL(9,6) NOT NULL,
+    longtitude DECIMAL(9,6) NOT NULL,
+    country_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (country_id) REFERENCES country(id)
+);
+
+CREATE TABLE mesurement(
+    id int NOT NULL AUTO_INCREMENT,
+    datetime DATETIME NOT NULL,
+    temperature DECIMAL(7,2) NOT NULL,
+    pressure DECIMAL(7,2) NOT NULL,
+    humidity  DECIMAL(7,2) NOT NULL,
+    temperature_min DECIMAL(7,2) NOT NULL,
+    temperature_max DECIMAL(7,2) NOT NULL,
+    weather_main VARCHAR(50) NOT NULL,
+    weather_desc VARCHAR(200) NOT NULL,
+    wind_speed DECIMAL(4,2) NOT NULL,
+    city_id INT NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (city_id) REFERENCES city(id)
+)
